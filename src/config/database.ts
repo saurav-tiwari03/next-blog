386150d1ea2e33969ab256e1cf210b1d64a18314
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2024-07-27 21:06:58 +0530

    connect

--- src/config/database.ts
+++ src/config/database.ts
@@ -13,8 +13,8 @@ async function dbConnect(): Promise<void> {
   }
 
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URL);
-    
+    const db = await mongoose.connect(process.env.MONGODB_URL || '', {});
+
     connection.isConnected = db.connections[0].readyState;
 
     console.log('Database connected successfully');
